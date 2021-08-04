7c20fb81d1b6943313f6aa53779b3b12ce119999
shaanripon123 <shaanrip8@gmail.com>
2021-08-04 23:57:15 +0600

    router-added

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,7 +7,7 @@ import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
-    const [products, setProdcts] = useState(first10);
+    const [products,] = useState(first10);
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) => {
